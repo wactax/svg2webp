087c5c9fd7da82658c57f379b89b5c6c20f48685
wacdev <i@wac.tax>
2023-05-10 12:05:34 +0800

    ♨

--- dev.sh
+++ dev.sh
@@ -12,6 +12,6 @@ fi
 export RUSTFLAGS='--cfg reqwest_unstable'
 export RUST_LOG=$RUST_LOG,watchexec=off,watchexec_cli=off,globset=warn
 
-exec bunx concurrently --kill-others --raw -- \
+bunx concurrently --kill-others --raw -- \
   "watchexec -n --project-origin . -w ./coffee --exts coffee -- ./test.sh" \
   "watchexec -pn --project-origin . -w ./src --exts rs,toml -r -- ./run.sh"
