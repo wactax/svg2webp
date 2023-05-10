2bdb9130f7aea7eb93666fe9190bdc9da2f1c045
wacdev <i@wac.tax>
2023-05-10 11:58:10 +0800

    ♨

--- dev.sh
+++ dev.sh
@@ -2,7 +2,12 @@
 
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
+source sh/pid.sh
 set -ex
+
+if [ ! -d "node_modules" ]; then
+  yarn install
+fi
 
 export RUSTFLAGS='--cfg reqwest_unstable'
 export RUST_LOG=$RUST_LOG,watchexec=off,watchexec_cli=off,globset=warn
